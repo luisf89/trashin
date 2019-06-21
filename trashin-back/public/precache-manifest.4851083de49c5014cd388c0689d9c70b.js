580b2b6484c1bbd0cd9f26877d2337a90c20d0cf
luisf89 <luisf.martinez89@gmail.com>
2019-06-20 20:58:33 -0500

    remove npm

---
+++ trashin-back/public/precache-manifest.4851083de49c5014cd388c0689d9c70b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106d7afb32a34058d659e0009689a639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8480b691c7be13329d7c",
+    "url": "/static/css/2.858b5ee9.chunk.css"
+  },
+  {
+    "revision": "21273b55b0dfa5901b7f",
+    "url": "/static/css/main.0b685f63.chunk.css"
+  },
+  {
+    "revision": "8480b691c7be13329d7c",
+    "url": "/static/js/2.ad5d4dcd.chunk.js"
+  },
+  {
+    "revision": "21273b55b0dfa5901b7f",
+    "url": "/static/js/main.ff600cfd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86ceb9409db74ccb4a56a744e1c469b5",
+    "url": "/static/media/Green.86ceb940.png"
+  },
+  {
+    "revision": "e6bbfeecff74745223681834b4be851c",
+    "url": "/static/media/Recycling.e6bbfeec.png"
+  },
+  {
+    "revision": "3e468806855caa34733db5f37157beba",
+    "url": "/static/media/Skyline.3e468806.png"
+  },
+  {
+    "revision": "4577b225e869f3f8a27081c21afb76ab",
+    "url": "/static/media/air.4577b225.jpg"
+  },
+  {
+    "revision": "a977040bbbb088b9b5869fad90b3f7ae",
+    "url": "/static/media/bellota.a977040b.jpg"
+  },
+  {
+    "revision": "99a37a633a3225fbfe1f7b5a378612f9",
+    "url": "/static/media/river.99a37a63.jpg"
+  },
+  {
+    "revision": "ea91bf09521078fa56cd47c8fb11d681",
+    "url": "/static/media/smile.ea91bf09.jpg"
+  }
+]);
